2331cbaa52df66ababc4428e137bd32f2a0ea57d
bell <bellhyunforgithub@gmail.com>
2025-11-13 01:18:45 +0900

    refact: 비밀번호 초기화 토큰과 유저 다대일로 해결

--- mpl-domain/src/main/java/com/codeit/sb02mplteam2/domain/auth/entity/PasswordResetToken.java
+++ mpl-domain/src/main/java/com/codeit/sb02mplteam2/domain/auth/entity/PasswordResetToken.java
@@ -1,14 +1,8 @@
 package com.codeit.sb02mplteam2.domain.auth.entity;
 
 import com.codeit.sb02mplteam2.domain.user.entity.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,7 +21,7 @@ public class PasswordResetToken {
   @Column(nullable = false, unique = true)
   private String token;
 
-  @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+  @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
   @JoinColumn(nullable = false, name = "user_id")
   private User user;
 
--- mpl-domain/src/main/java/com/codeit/sb02mplteam2/domain/auth/repository/PasswordResetTokenRepository.java
+++ mpl-domain/src/main/java/com/codeit/sb02mplteam2/domain/auth/repository/PasswordResetTokenRepository.java
@@ -1,10 +1,13 @@
 package com.codeit.sb02mplteam2.domain.auth.repository;
 
 import com.codeit.sb02mplteam2.domain.auth.entity.PasswordResetToken;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
   Optional<PasswordResetToken> findByToken(String token);
-  Optional<PasswordResetToken> findByUserId(Long userId);
+  List<PasswordResetToken> findByUserId(Long userId);
+  void deleteAllByUserId(Long userId);
 }
